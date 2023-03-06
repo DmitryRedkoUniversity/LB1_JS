d05e9e9d556ed9dc50a5f28b2331f1450dd004b5
DmitryRedko <dar27@tpu.ru>
2023-03-06 15:33:11 +0300

    first_lb_finished

--- LB1/10.js
+++ LB1/10.js
@@ -1,26 +1,24 @@
 function word_edit(mass){
-    var sum=0;
     for(let i=0; i<mass.length; i++){
         mass[i]=mass[i].charAt(0).toUpperCase()+mass[i].slice(1).toLowerCase();
     }
     return mass
-    }
+}
 
 var button = document.getElementById('But');
 button.addEventListener("click",main);
 
 
 function main(){
-    var str = document.getElementById('Arr').value;
-    var out = document.getElementById('out');
-    
-    var regex = /^[а-яa-zА-ЯA-Z\s]+$/; 
-    if(regex.test(str) === false) {
-        out.value="Error: enter words consisting of a-z(A-Z) or а-я(А-Я)";
-    } else {
+    let str = document.getElementById('Arr').value;
+    let out = document.getElementById('out');
+    let regex = /^[а-яa-zА-ЯA-Z\s]+$/; 
+
+    if(regex.test(str) === true) {
         var mass = str.split(" ");
         out.value=word_edit(mass);
-
-
+    } else {
+        out.value="Error: enter words consisting of a-z(A-Z) or а-я(А-Я)";
     }
 }
+
