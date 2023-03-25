a2622700d14296e77f4ce9dc58f4eab935763897
DmitryRedko <dar27@tpu.ru>
2023-03-25 09:25:02 +0700

    LB4

---
+++ LB4/10.js
@@ -0,0 +1,21 @@
+function consecutive(num) {
+    if (!Number.isInteger(num) || num < 2) {
+        return false;
+    }
+
+    for (let a1 = 1; a1 < num / 2; a1++) {
+        n = (1 - 2 * a1 + Math.sqrt(Math.pow((2 * a1 - 1), 2) + 8 * num)) / 2;
+        if (Math.trunc(n) == n) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function main() {
+    let inputNumber = Number(document.getElementById('input').value.trim());
+    document.getElementById('output').value = consecutive(inputNumber);
+}
+
+let button = document.getElementById('button');
+button.addEventListener("click", main);
